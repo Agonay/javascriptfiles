c9dabd42fac6457fa86746593664a43881d51f5c
Agonay <agrosocas@gmail.com>
2019-10-12 20:23:03 +0200

    Add files via upload

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+(function() {
+    let secondary = document.getElementById('secondary');
+    /*document.getElementyById('secondary').style.display="none" */
+    secondary.style.display="none";
+
+})();
